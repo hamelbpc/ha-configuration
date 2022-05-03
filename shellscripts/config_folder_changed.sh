b210b3fb2e06b4f823455d6300894a1c107b46cf
root <root@homeassistant.local.hass.io>
2022-05-03 13:57:40 +1000

    changes made in home assistant

--- shellscripts/config_folder_changed.sh
+++ shellscripts/config_folder_changed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /config
-git config core.sshCommand '-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
+git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
 git add .
 git commit -m "changes made in home assistant"
 git push origin main
