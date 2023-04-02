f997459623e1c9cbd74c5f4c47fb686408a662e1
root <root@homeassistant.local.hass.io>
2023-04-02 12:59:09 +1000

    changes made in home assistant

--- shellscripts/theageforecast.sh
+++ shellscripts/theageforecast.sh
@@ -2,5 +2,6 @@
 
 apk add htmlq --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 curl --silent https://weather.theage.com.au/local-forecast/vic/melbourne | htmlq "#ga_48_hours svg" | tee /config/www/theageforecast.svg
+sed -r 's/xlink:href/href/g' /config/www/theageforecast.svg
 
 
